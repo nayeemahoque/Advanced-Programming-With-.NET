ac9dda53388f8887e0c33975b2e2af31f5de308c
nayeemahoque <113884690+nayeemahoque@users.noreply.github.com>
2023-01-27 16:43:31 +0600

    Basic Profile

---
+++ MyProfile/MyProfile/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyProfile.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyProfile/MyProfile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyProfile.MvcApplication" Language="C#" %>
