ef6c220bf30ec596cbc17face7931b32002af41f
nayeemahoque <113884690+nayeemahoque@users.noreply.github.com>
2023-01-31 00:06:25 +0600

    Static profile Creation

--- MyProfile/MyProfile/App_Start/RouteConfig.cs
+++ MyProfile/MyProfile/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyProfile
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Profile", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Profile", action = "MyProfile", id = UrlParameter.Optional }
             );
         }
     }
