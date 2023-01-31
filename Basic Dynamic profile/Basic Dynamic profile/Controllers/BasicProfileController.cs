01ac0c230f439b509ef35b82d5967d9b7308cf5c
nayeemahoque <113884690+nayeemahoque@users.noreply.github.com>
2023-02-01 03:44:11 +0600

    Data Passing and Layout

--- Basic Dynamic profile/Basic Dynamic profile/Controllers/BasicProfileController.cs
+++ Basic Dynamic profile/Basic Dynamic profile/Controllers/BasicProfileController.cs
@@ -11,23 +11,36 @@ namespace Basic_Dynamic_profile.Controllers
         // GET: BasicProfile
         public ActionResult Home()
         {
+            ViewBag.Title = "Home";
+            ViewBag.Name = "Nayeema";
+            ViewBag.ID = "20-43737-2";
             return View();
         }
         public ActionResult About()
         {
+            ViewBag.Title = "About";
             return View();
         }
         public ActionResult Contact()
         {
+            ViewBag.Title = "Contact";
             return Redirect("https://dotnet.microsoft.com/en-us/apps/aspnet");
         }
         public ActionResult Login()
         {
+            ViewBag.Title = "Login";
             return View();
         }
         public ActionResult signup()
         {
+            ViewBag.Title = "SignUp";
             return View();
         }
+        public ActionResult LoginSubmit()
+        {
+            TempData["Msg"] = "Login Successfull";
+            return RedirectToAction("MyProfile, DashboardProfile");
+        }
+
     }
 }
---
+++ Basic Dynamic profile/Basic Dynamic profile/Controllers/DashboardProfileController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Basic_Dynamic_profile.Controllers
+{
+    public class DashboardProfileController : Controller
+    {
+        // GET: DashboardProfile
+        public ActionResult MyProfile()
+        {
+            TempData["Msg"] = "Login Successfull";
+            return View();
+        }
+        public ActionResult Settings()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Home", "BasicProfile");
+        }
+    }
+}
