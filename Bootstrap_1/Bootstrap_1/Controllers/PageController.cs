61111987e75e86023e7f15a1d9bff56db7b565a6
nayeemahoque <113884690+nayeemahoque@users.noreply.github.com>
2023-02-04 23:52:49 +0600

    Table Styles WIP

---
+++ Bootstrap_1/Bootstrap_1/Controllers/PageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap_1.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FirstPage()
+        {
+            return View();
+        }
+        public ActionResult SecondPage()
+        {
+            return View();
+        }
+        public ActionResult ThirdPage()
+        {
+            return View();
+        }
+    }
+}
